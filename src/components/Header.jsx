0ce57fb6bbd56ee2e35eda737a84f9af87c8cb5f
Gohar <goharwah786@gmail.com>
2025-05-19 15:12:35 -0500

    New Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import BeechersLogo from '../assets/beecherslogo.png';
 
 function Header() {
@@ -17,7 +16,7 @@ function Header() {
       <div className="mt-3 md:hidden text-center">
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className="cursor-pointer border border-white rounded whiteHeaderHover transition duration-500"
+          className="cursor-pointer border border-white rounded whiteHeaderHover transition duration-300"
         >
           <span>
             <img
@@ -31,25 +30,25 @@ function Header() {
         </button>
 
         <div
-          className={`transition-all overflow-hidden duration-500 ease-in-out ${
+          className={`transition-all overflow-hidden duration-300 ease-in-out ${
             isOpen ? 'max-h-60' : 'max-h-0'
           }`}
         >
           <ul className="flex flex-col items-center space-y-2 bg-[#c2093b] p-4 rounded shadow-md">
-            <li><Link to="/" className="whiteHeaderHover">HOME</Link></li>
-            <li><Link to="/menu" className="whiteHeaderHover">OUR MENU</Link></li>
-            <li><Link to="/contact" className="whiteHeaderHover">CONTACT US</Link></li>
-            <li className='-mb-3'><Link to="/application" className="whiteHeaderHover">APPLICATION</Link></li>
+            <li><a href="/" className="whiteHeaderHover">HOME</a></li>
+            <li><a href="/menu" className="whiteHeaderHover">OUR MENU</a></li>
+            <li><a href="/contact" className="whiteHeaderHover">CONTACT US</a></li>
+            <li className='-mb-3'><a href="/application" className="whiteHeaderHover">APPLICATION</a></li>
           </ul>
         </div>
       </div>
 
       <nav className="mt-4 font-[550] hidden md:block">
         <ul className="flex justify-center space-x-4">
-          <li><Link to="/" className="whiteHeaderHover">HOME</Link></li>
-          <li><Link to="/menu" className="whiteHeaderHover">OUR MENU</Link></li>
-          <li><Link to="/contact" className="whiteHeaderHover">CONTACT US</Link></li>
-          <li><Link to="/application" className="whiteHeaderHover">APPLICATION</Link></li>
+          <li><a href="/" className="whiteHeaderHover">HOME</a></li>
+          <li><a href="/menu" className="whiteHeaderHover">OUR MENU</a></li>
+          <li><a href="/contact" className="whiteHeaderHover">CONTACT US</a></li>
+          <li><a href="/application" className="whiteHeaderHover">APPLICATION</a></li>
         </ul>
       </nav>
     </header>
